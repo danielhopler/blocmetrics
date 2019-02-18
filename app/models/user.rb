1df1651f7f2f87d772169addf2ea91fd2bbe35ed
Daniel Hopler <hopler.daniel@gmail.com>
2019-02-18 17:43:25 -0500

    Add sign in and sign out feature. Add username attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,34 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+  attr_accessor :login
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+    conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_hash).first
+  end
+
 end
