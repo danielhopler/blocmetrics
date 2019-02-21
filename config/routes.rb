e2756e67cc4f692ce40aa321b6d6e5f64e4aa0d1
Daniel Hopler <hopler.daniel@gmail.com>
2019-02-20 22:23:08 -0500

    Added events API

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   resources :registered_applications
 
   devise_for :users
-  
+
+  namespace :api, defaults: { format: :json } do
+     match '/events', to: 'events#preflight', via: [:options]
+     resources :events, only: [:create]
+   end
+
   get 'welcome/index'
   get 'welcome/about'
 
